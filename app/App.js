15c0e9fa683f0f50ad198a3175c25c48028ca93b
GersenJPJorge <gersen@ibest.com.br>
2017-05-19 18:39:44 -0300

    Add files via upload

---
+++ app/App.js
@@ -0,0 +1,11 @@
+
+var React = require('react'); // importa a lib react
+var ReactDOM = require('react-dom'); // importa a lib react-dom 
+
+ReactDOM.render(<h1>Hello World</h1>, document.getElementById('app'));
+
+var MeuComponente = require('./components/MeuComponente.js');
+
+
+
+
---
+++ app/components/MeuComponente.js
@@ -0,0 +1,13 @@
+var React = require('react'); // importa a lib react
+
+var MeuComponente = React.createClass({
+  render: function(){
+    return (
+      
+  teste
+
+    );
+  }
+});
+
+module.exports = MeuComponente;
